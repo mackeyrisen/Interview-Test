352d155fe2198853ad40ddf363476e037ae86ac1
mackeyrisen <mackeyrisen@gmail.com>
2026-03-26 22:11:10 +0700

    Add project.nuget.cache file for Interview-Test.GatewayApi with package dependencies

--- Interview-Test/Interview-Test.GatewayApi/Program.cs
+++ Interview-Test/Interview-Test.GatewayApi/Program.cs
@@ -1,15 +1,16 @@
+using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Configuration.AddJsonFile("configurationOcelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,5 +19,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseOcelot();
+await app.UseOcelot();
 app.Run();
